63b8ecbf3e1ffa134c68d7fee36ba289ccaecc94
ferny <116464667+fernyrepos@users.noreply.github.com>
2025-04-06 05:26:48 -0400

    Welcome to Git

---
+++ 1.4/Source/EasyCaravanFormationMod.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using UnityEngine;
+using Verse;
+
+namespace EasyCaravanFormation
+{
+    public class EasyCaravanFormationMod : Mod
+    {
+        public EasyCaravanFormationMod(ModContentPack pack) : base(pack)
+        {
+			new Harmony("EasyCaravanFormationMod").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Dialog_FormCaravan), "TrySend")]
+    public static class Dialog_FormCaravan_TrySend_Patch
+    {
+        public static bool Prefix(Dialog_FormCaravan __instance)
+        {
+            if (GenHostility.AnyHostileActiveThreatToPlayer(__instance.map) is false)
+            {
+                if (__instance.DebugTryFormCaravanInstantly())
+                {
+                    __instance.Close();
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
